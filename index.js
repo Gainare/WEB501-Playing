15afc8cb7c7ee6ee8bc4d7d98eda40e62161dcc9
Gainare <aaron-hemi@live.nmit.ac.nz>
2020-09-17 10:52:06 +1200

    created and adding JS functions

---
+++ index.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("input");
+var ul = document.querySelector("ul");
+
+function inputLength();{
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document)
+}
